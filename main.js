eaec7985a5dac26b21aa5df13efabc1b3302bc6d
agftech <agiraof@gmail.com>
2022-11-03 17:46:25 -0700

    :hammer: refactor: improve code

--- main.js
+++ main.js
@@ -25,18 +25,12 @@ function createCard(date, day, games) {
   `;
 }
 
-document.querySelector("#app").innerHTML = `
-<header>
-  <img src="./assets/logo.svg" alt="Logo da NLW Copa" />
-</header>
-<main id="cards">
-  ${createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia"))}
-  ${createCard(
+document.querySelector("#cards").innerHTML = 
+  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+  createCard(
     "28/11",
     "segunda",
     createGame("brazil", "13:00", "switzerland") +
       createGame("portugal", "16:00", "uruguay")
-  )}
-  ${createCard("02/12", "sexta", createGame("cameroon", "16:00", "brazil"))}
-</main>
-`;
+  ) +
+  createCard("02/12", "sexta", createGame("cameroon", "16:00", "brazil"));
